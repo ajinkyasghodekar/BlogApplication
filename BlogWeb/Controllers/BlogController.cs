24c29961db069577bc12528121d8f849052aed35
ajinkyasghodekar <ajinkyasghodekar@gmail.com>
2023-06-01 11:49:12 +0530

    Added Edit and Delete operations for Blog in MVC

--- BlogWeb/Controllers/BlogController.cs
+++ BlogWeb/Controllers/BlogController.cs
@@ -14,6 +14,7 @@ namespace BlogWeb.Controllers
             _db = db;
         }
 
+
         // View all blogs in UI
         public IActionResult Index()
         {
@@ -35,5 +36,61 @@ namespace BlogWeb.Controllers
                 return RedirectToAction("Index");           
         }
 
+
+        // Edit a blog in UI
+        public IActionResult EditBlog(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Blog? blogFromDb = _db.BlogsTable.Find(id);
+
+            if (blogFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(blogFromDb);
+        }
+        [HttpPost]
+        public IActionResult EditBlog(Blog obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.BlogsTable.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+        // Edit a blog in UI
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Blog? blogFromDb = _db.BlogsTable.Find(id);
+
+            if (blogFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(blogFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Blog? obj = _db.BlogsTable.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.BlogsTable.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
